d16e0bb1da95a5429c0ca9888f1457583b81dd98
Elumenix <dps5393@g.rit.edu>
2023-09-29 10:52:14 -0400

    got index and css working

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
   console.log(request.url);
+
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/style.css':
+      htmlHandler.getCSS(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {

bd53bfec419d75e994ff080d73dd847ed3257dfb
Elumenix <dps5393@g.rit.edu>
2023-09-30 16:49:36 -0400

    Got post requests to work

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,5 +1,41 @@
 const users = {};
 
+const updateUsers = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
+
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    response.writeHead(400, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify(responseJSON));
+    response.end();
+    return response;
+  }
+
+  let responseCode = 204;
+
+  if (!users[body.name]) {
+    responseCode = 201;
+    users[body.name] = {};
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    response.writeHead(responseCode, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify(responseJSON));
+    response.end();
+    return response;
+  }
+
+  response.writeHead(responseCode, { 'Content-Type': 'application/json' });
+  response.end();
+  return response;
+};
+
 const getUsers = (request, response) => {
   response.writeHead(200, { 'Content-Type': request.headers.accept });
 
@@ -16,7 +52,8 @@ const getUsers = (request, response) => {
 const getUsersMeta = (request, response) => {
   response.writeHead(200, { 'Content-Type': request.headers.accept });
   response.end();
-}
+};
 
 module.exports.getUsers = getUsers;
 module.exports.getUsersMeta = getUsersMeta;
+module.exports.updateUsers = updateUsers;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
 
@@ -16,6 +17,9 @@ const urlStruct = {
     '/getUsers': jsonHandler.getUsersMeta,
     notFound: htmlHandler.getIndex,
   },
+  POST: {
+    '/userPath': jsonHandler.updateUsers,
+  }
 };
 
 const onRequest = (request, response) => {
@@ -28,11 +32,37 @@ const onRequest = (request, response) => {
   }
 
   if (urlStruct[request.method][pathname]) {
-    return urlStruct[request.method][pathname](request, response);
+    if (request.method === 'POST') {
+      return handlePost(request, response, pathname);
+    }
+    else {
+      return urlStruct[request.method][pathname](request, response);
+    }
   }
 
   return urlStruct[request.method].notFound(request, response);
 };
+
+const handlePost = async (request, response, pathname) => {
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    return urlStruct[request.method][pathname](request, response, bodyParams);
+  });
+}
 
 http.createServer(onRequest).listen(port, () => {
   console.log(`Listening on 127.0.0.1:${port}`);
